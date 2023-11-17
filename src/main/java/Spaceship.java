d63cfc1745b6e39edefdb39572aa9bb24c77f5e7
Tiago Oliveira <tiago.alexandre.oliveira.02@gmail.com>
2023-11-17 21:03:20 +0000

    Spaceship class created with some content.

--- src/main/java/Spaceship.java
+++ src/main/java/Spaceship.java
@@ -1,4 +1,29 @@
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
 public class Spaceship {
+    private Position position;
 
+    public Spaceship(Position position){
+        this.position = position;
+    }
 
+    public Position getPosition(){
+        return position;
+    }
+
+    public void setPosition(Position position){
+        this.position = position;
+    }
+
+    public void draw(TextGraphics graphics){
+        int x = position.getX();
+        int y = position.getY();
+
+        graphics.setForegroundColor(TextColor.Factory.fromString("#FF3333"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.putString(new TerminalPosition(position.getX(), position.getY()),"S");
+    }
 }
