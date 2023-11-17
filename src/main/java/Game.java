6b5d7fa81ef8801f233a06cb289d4cecc5986ec6
Tiago Oliveira <tiago.alexandre.oliveira.02@gmail.com>
2023-11-17 09:27:53 +0000

    New start menu.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,9 +1,15 @@
+import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.gui2.*;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
+import com.googlecode.lanterna.gui2.TextGUI;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
-import com.googlecode.lanterna.terminal.TerminalFactory;
+import com.googlecode.lanterna.terminal.TerminalResizeListener;
 
 import java.io.IOException;
 
@@ -11,37 +17,62 @@ public class Game {
 
     public static void main(String[] args) {
         try {
-            //Initializing the terminal and screen
-            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            TerminalSize terminalSize = new TerminalSize(50, 20);
+
+            // Initializing the terminal
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setTerminalEmulatorTitle("Savior of the Solar System");
+            terminalFactory.setInitialTerminalSize(terminalSize);
+            Terminal terminal = terminalFactory.createTerminal();
+
+            // Initializing the screen
             TerminalScreen screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null);
             screen.startScreen();
 
-            //Initialize the GUI with the screen to display our name
-            MultiWindowTextGUI gui = new MultiWindowTextGUI(screen);
-            Window window = new BasicWindow("Savior of the Solar System");
 
-            //Panel to hold components
-            Panel contentPanel = new Panel();
-            contentPanel.setLayoutManager(new GridLayout(1));
-            contentPanel.addComponent(new Label("Welcome, our Savior"));
-            contentPanel.addComponent(new EmptySpace(new TerminalSize(0, 1)));
+            // Set the default background color to black
+            terminal.setBackgroundColor(TextColor.ANSI.BLACK);
+            terminal.setForegroundColor(TextColor.ANSI.WHITE);
+            terminal.clearScreen();
 
-            //"Start" button, but since we don't have anything yet it just closes
-            Button startButton = new Button("Start", () -> {
-                try {
-                    screen.stopScreen();
-                } catch (IOException e) {
-                    e.printStackTrace();
+            // Centralize the terminal window
+            int terminalWindowX = (terminalSize.getColumns() - 30) / 2;
+            int terminalWindowY = (terminalSize.getRows() - 5) / 2;
+
+            // Create a TextGUI to handle user input
+            TextGUI textGUI = new MultiWindowTextGUI(screen);
+
+            // Draw some "components" of the menu
+            drawTitleAndMessage(screen, terminalWindowX, terminalWindowY);
+            drawButton(screen, terminalWindowX + 10, terminalWindowY + 5, "Start");
+            screen.refresh();
+
+            // Wait for the user to press Enter
+            while (true) {
+                KeyStroke keyStroke = terminal.pollInput();
+                if (keyStroke != null && keyStroke.getKeyType() == KeyType.Enter) {
+                    screen.close();
+                    break;
                 }
-            });
+            }
 
-            //Add the "Start" button to the panel
-            contentPanel.addComponent(startButton);
-            window.setComponent(contentPanel);
-            gui.addWindowAndWait(window);
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private static void drawTitleAndMessage(TerminalScreen screen, int x, int y) {
+        TextGraphics textGraphics = screen.newTextGraphics();
+        textGraphics.setForegroundColor(TextColor.ANSI.WHITE);
+        textGraphics.putString(x, y, "Savior of the Solar System");
+        textGraphics.putString(x, y + 2, "Welcome, our Savior");
+        textGraphics.putString(x, y + 3, "Press Enter to start playing!");
+    }
+
+    private static void drawButton(TerminalScreen screen, int x, int y, String label) {
+        TextGraphics textGraphics = screen.newTextGraphics();
+        textGraphics.setForegroundColor(TextColor.ANSI.WHITE);
+        textGraphics.setBackgroundColor(TextColor.ANSI.BLACK);
+        textGraphics.putString(x, y, "[" + label + "]");
+    }
 }
