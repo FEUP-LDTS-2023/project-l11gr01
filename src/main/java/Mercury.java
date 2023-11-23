484343e584c65e9bf17f76964a8668c873503179
Tiago Martins <up202206640@up.pt>
2023-11-23 21:33:06 +0000

    Added Element abstract Class

--- src/main/java/Mercury.java
+++ src/main/java/Mercury.java
@@ -1,45 +1,9 @@
-import com.googlecode.lanterna.TerminalPosition;
-import com.googlecode.lanterna.TextColor;
-import com.googlecode.lanterna.graphics.TextGraphics;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
-import com.googlecode.lanterna.TerminalSize;
 
-import java.io.IOException;
+import com.googlecode.lanterna.graphics.TextGraphics;
 
 public class Mercury extends Planet {
-
-    private TerminalScreen screen;
-    private static final TextColor backgroundColor = TextColor.Factory.fromString("#536872");
-
-    public Mercury() {
-        try {
-            TerminalSize terminalSize = new TerminalSize(90, 45);
-
-            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setTerminalEmulatorTitle("Saviors of the Solar System");
-            terminalFactory.setInitialTerminalSize(terminalSize);
-            Terminal terminal = terminalFactory.createTerminal();
-
-            screen = new TerminalScreen(terminal);
-            screen.setCursorPosition(null);
-            screen.startScreen();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void drawFloor() {
-        try{
-        TextGraphics graphics = screen.newTextGraphics();
-        graphics.setBackgroundColor(backgroundColor);
-        graphics.setForegroundColor(backgroundColor);
-        graphics.fillRectangle(new TerminalPosition(0, 0), screen.getTerminalSize(), ' ');
-        screen.refresh();
-
-    } catch (IOException e){
-            e.printStackTrace();
-        }
+    public Mercury(TextGraphics graphics){
+        //Create Planet and start new Spaceship.
+        super(graphics, "#FFFFFF", new Spaceship(45,20));
     }
 }
--- src/main/java/Planet.java
+++ src/main/java/Planet.java
@@ -1,4 +1,46 @@
-public abstract class Planet {
-    public abstract void drawFloor();
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
+import java.io.IOException;
 
+public abstract class Planet{
+
+    protected TextColor backgroundColor;
+    protected Spaceship spaceship;
+
+    public Planet(TextGraphics graphics, String backgroundColor, Spaceship spaceship){
+        this.backgroundColor = TextColor.Factory.fromString(backgroundColor);
+        this.spaceship = spaceship;
+        draw(graphics);
+    }
+
+    public void draw(TextGraphics graphics) {
+        //Set Planet's background color
+        graphics.setBackgroundColor(backgroundColor);
+        graphics.setForegroundColor(backgroundColor);
+        graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(90,45), ' ');
+
+        spaceship.draw(graphics, "S");
+    }
+
+    void processKey(KeyStroke keyStroke) throws IOException {
+        if(keyStroke != null){
+            switch(keyStroke.getKeyType()){
+                case ArrowUp:
+                    spaceship.moveUp();
+                    break;
+                case ArrowDown:
+                    spaceship.moveDown();
+                    break;
+                case ArrowLeft:
+                    spaceship.moveLeft();
+                    break;
+                case ArrowRight:
+                    spaceship.moveRight();
+                    break;
+            }
+        }
+    }
 }
